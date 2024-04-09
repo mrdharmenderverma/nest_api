6ec730ee44cb45a1e71a875fcb434c7d24e60942
imdharmenderverma <dharmender.v@infinitysoftsystems.com>
2024-04-09 13:02:55 +0530

    fix default error exception

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -2,6 +2,7 @@ import { Category } from "../sehemas/book.schema";
 import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateBookDto {
+
     @IsNotEmpty()
     @IsString()
     readonly title: string;
@@ -18,8 +19,8 @@ export class CreateBookDto {
     @IsNumber()
     readonly price: number;
     
-    // @IsNotEmpty()
-    // @IsEnum(Category, { message: 'Please enter correct category' })
+    @IsNotEmpty()
+    @IsEnum(Category, { message: 'Please enter correct category' })
     readonly category: Category;
 }
 
